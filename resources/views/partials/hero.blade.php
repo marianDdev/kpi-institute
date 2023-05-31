92aa300d48b804846254ce0fe25aa1cf52359720
Marian Dumitru <d.mariandev@gmail.com>
2023-05-31 22:24:58 +0300

    navbar and hero

---
+++ resources/views/partials/hero.blade.php
@@ -0,0 +1,15 @@
+<section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <h1 class="mb-5 text-3xl font-bold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">The KPI Dictionary Volume I: Functional Areas</h1>
+        <hr class="w-full h-1 bg-green-400 dark:bg-green">
+        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">3.200+ Key Performance Indicator Definitions <br> for an in depth view on performance measurement</p>
+        <hr class="w-full h-1 bg-green-400 dark:bg-green">
+        <div class="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+            <div class="flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
+             <p>
+                 The KPI Dictionary - Volume 1 is a reference collection of Key Performance Indicators, used in practice today, which covers the major functional areas of an organization, regardless of its industry specifics. The collection contains the most relevant means for measuring performance, throughout the most common departments in either public, private or non-profit organizations, such as: Finance, Sales, Human Resources, Marketing and IT.
+             </p>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,23 @@
+
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" class="flex items-center">
+            <img src="{{ url('/images/logo.png')  }}" class="h-8 mr-3" alt="TKI Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap text-green-400">TKI RESEARCH LIBRARY</span>
+        </a>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
+                </li>
+                <li>
+                    |
+                </li>
+                <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Register</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<hr class="w-full h-2 bg-green-400 dark:bg-green">
